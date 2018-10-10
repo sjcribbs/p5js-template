1c0a5022817255121102aa05a6f98101053fa402
nbriz <nickbriz@gmail.com>
2018-10-10 07:27:19 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# p5js-template
-basic p5js template
+# A p5js Template
+
+### instructions
+
+1. click on the green "clone or download" button && copy the URL to clone this project. in ur terminal enter:
+```bash
+git clone https://github.com/net-art-and-cultures/p5js-template.git
+```
+
+2. cd into the p5js-template folder && run a local server
+```bash
+cd p5js-template
+```
+```bash
+python -m SimpleHTTPServer
+```
+
+3. request the page from ur browser
+```
+localhost:8000
+```
---
+++ js/main.js
@@ -0,0 +1,30 @@
+let catPic
+
+function preload(){
+    catPic = loadImage('../images/unicorn-cat.png')
+}
+
+function setup(){
+    createCanvas( innerWidth, innerHeight )
+
+    let catX = width/2 - catPic.width/2
+    let catY = height/2 - catPic.height/2
+    image( catPic, catX, catY )
+}
+
+function draw(){
+
+    if(mouseIsPressed){
+        let r = random(255)
+        let g = random(255)
+        let b = random(255)
+        fill( r, g, b )
+        for (let i = 0; i < 10; i++) {
+            let s = i*10
+            let y = mouseY + i*s/2
+            ellipse( mouseX, y, s )
+        }
+
+    }
+
+}
